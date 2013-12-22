bbfc145710511c1395e48d222ac636e49832d806
Michael Schulze <ms@michsch.de>
2013-12-22 17:49:06 +0100

    add footer and license information

--- dev/coffee/app.coffee
+++ dev/coffee/app.coffee
@@ -1,4 +1,4 @@
-###* jshint ###
+### jshint devel:true ###
 ### global
   define,
   exports,
@@ -30,6 +30,7 @@ define [
       #Hyphenator?.run()
 
       # Functions
+      @loadGithubProfile()
       prettyPrint?()
       true
 
@@ -48,6 +49,34 @@ define [
               htmlClasses[index] = 'js'
 
           html.className = htmlClasses.join ' '
+      true
+
+    loadGithubProfile : ->
+      @$githubProfile = $ '.github-profile'
+
+      if @$githubProfile
+        $.ajax
+          url: 'https://api.github.com/users/michsch',
+          dataType: 'json'
+          success: (data) =>
+            @buildGithubAvatar data
+          error: (data) =>
+            @$githubProfile.addClass 'error'
+      true
+
+    buildGithubAvatar : (data) ->
+      console.log data
+
+      if typeof data is 'object' and data.avatar_url
+        avatarUrl = data.avatar_url + '&s=440'
+
+        $img = $ '<img src="' + avatarUrl + '" alt="' +
+          'Github profile picture of ' + data.login + '">'
+
+        $link = $ '<a href="' + data.blog + '" title="' + 'Website of ' + data.login + '" itemprop="url">'
+
+        $link.append $img
+        @$githubProfile.append $link
       true
 
   class App
--- js/dev/app.js
+++ js/dev/app.js
@@ -1,4 +1,4 @@
-/** jshint*/
+/* jshint devel:true*/
 
 /* global
   define,
@@ -21,6 +21,7 @@ define(['jquery', 'module/accessifyhtml5', 'module/yaml-focusfix', 'antispammail
       accessifyHTML5();
       yaml_focusfix = new YAML_focusFix();
       yaml_focusfix.init();
+      this.loadGithubProfile();
       if (typeof prettyPrint === "function") {
         prettyPrint();
       }
@@ -42,6 +43,37 @@ define(['jquery', 'module/accessifyhtml5', 'module/yaml-focusfix', 'antispammail
           }
           html.className = htmlClasses.join(' ');
         }
+      }
+      return true;
+    };
+
+    DomReady.prototype.loadGithubProfile = function() {
+      var _this = this;
+      this.$githubProfile = $('.github-profile');
+      if (this.$githubProfile) {
+        $.ajax({
+          url: 'https://api.github.com/users/michsch',
+          dataType: 'json',
+          success: function(data) {
+            return _this.buildGithubAvatar(data);
+          },
+          error: function(data) {
+            return _this.$githubProfile.addClass('error');
+          }
+        });
+      }
+      return true;
+    };
+
+    DomReady.prototype.buildGithubAvatar = function(data) {
+      var $img, $link, avatarUrl;
+      console.log(data);
+      if (typeof data === 'object' && data.avatar_url) {
+        avatarUrl = data.avatar_url + '&s=440';
+        $img = $('<img src="' + avatarUrl + '" alt="' + 'Github profile picture of ' + data.login + '">');
+        $link = $('<a href="' + data.blog + '" title="' + 'Website of ' + data.login + '" itemprop="url">');
+        $link.append($img);
+        this.$githubProfile.append($link);
       }
       return true;
     };
